a9eb06332fb2060e265e3833d153cb28adc0374b
Alejandro Cardozo <uri.cardozo@gmail.com>
2019-12-06 16:56:03 -0300

    POST new movie logic created

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,16 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello there! Shrek here.");
+  res.render("index.ejs");
+});
+
+router.get("/new-movie", (req, res) => {
+  res.render("new-movie");
+});
+
+router.post("/new-movie", (req, res) => {
+  console.log(req.body);
+  res.send("received");
 });
 
 module.exports = router;

0e3dd3dd35bb8ecac711ed900c0e5c5f95b5fc0c
Alejandro Cardozo <uri.cardozo@gmail.com>
2019-12-06 19:19:33 -0300

    data persistence in a JSON file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,15 @@
 const { Router } = require("express");
 const router = Router();
+const fs = require("fs");
+const uuid = require("uuid/v4");
+
+const json_movies = fs.readFileSync("src/movies.json", "utf-8");
+let movies = JSON.parse(json_movies);
 
 router.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {
+    movies
+  });
 });
 
 router.get("/new-movie", (req, res) => {
@@ -10,8 +17,29 @@ router.get("/new-movie", (req, res) => {
 });
 
 router.post("/new-movie", (req, res) => {
-  console.log(req.body);
-  res.send("received");
+  const { title, director, image, description } = req.body;
+  if (!title || !director || !image || !description) {
+    res.status(400).send("Please complete each field in the form");
+    return;
+  }
+  let newMovie = {
+    id: uuid(),
+    title,
+    director,
+    image,
+    description
+  };
+  movies.push(newMovie);
+  const json_movies = JSON.stringify(movies);
+  fs.writeFileSync("src/movies.json", json_movies, "utf-8");
+  res.redirect("/");
+});
+
+router.get("/delete/:id", (req, res) => {
+  movies = movies.filter(movie => movie.id != req.params.id);
+  const json_movies = JSON.stringify(movies);
+  fs.writeFileSync("src/movies.json", json_movies, "utf-8");
+  res.redirect("/");
 });
 
 module.exports = router;
